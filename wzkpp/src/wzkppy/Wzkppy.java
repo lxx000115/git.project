bb44173f18ae0d45d1f8cc2d76147d263c2fa48c
lxx000115 <74647085+lxx000115@users.noreply.github.com>
2020-11-30 21:47:00 +0800

    Update Wzkppy.java

--- wzkpp/src/wzkppy/Wzkppy.java
+++ wzkpp/src/wzkppy/Wzkppy.java
@@ -1,11 +1,11 @@
 package wzkppy;
 
 public class Wzkppy {
-	private int id;
-	private String username;
-	private String password;
-	private int sex;
-	private String love;
+	private int id;  //用户
+	private String username; //用户名
+	private String password;  //密码
+	private int sex;  //性别
+	private String love;  //爱好
 	
 	
 	public Wzkppy(int id, String username, String password, int sex, String love) {
